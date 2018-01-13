4fcb9e09a2248fd9b28aeb8793a72347120ee4b7
Dan Watling <daniel.watling@gmail.com>
2018-01-13 06:46:48 -0600

    Episode 2 - Adding Material

--- src/home.component.js
+++ src/home.component.js
@@ -1,7 +1,19 @@
 var controller = function($scope) {
+	$scope.newDescription = "";
+
 	this.$onInit = function() {
 		console.log("Hello");
 	};
+
+	$scope.onKeyPress = function($event) {
+		if ($event.keyCode === 13) {
+			$scope.onClickAdd();
+		}
+	}
+
+	$scope.onClickAdd = function() {
+		$scope.newDescription = "";
+	}
 }
 
 export default {
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import angular from 'angular';
 import 'angular-route';
+import 'angular-messages';
+import 'angular-aria';
+import 'angular-animate';
+import 'angular-material';
 
 import routes from './routes';
 import homeComponent from './home.component';
 
-var app = angular.module("todo", ['ngRoute']);
+var app = angular.module("todo", ['ngRoute', 'ngMaterial']);
 app.config(routes);
 app.component('home', homeComponent);
