ddcd4ffdaa7c2586bf03415ec12164158fe60358
Dan Watling <daniel.watling@gmail.com>
2018-01-18 06:21:32 -0600

    Episode 3 - Tying into backend

--- src/home.component.js
+++ src/home.component.js
@@ -1,9 +1,17 @@
-var controller = function($scope) {
+var controller = function($scope, $http) {
 	$scope.newDescription = "";
+	$scope.data = [];
 
 	this.$onInit = function() {
-		console.log("Hello");
+		$scope.refresh();
 	};
+
+	$scope.refresh = function() {
+		$http.get("http://localhost:8080/todo").then(function(response) {
+			console.log(response.data);
+			$scope.data = response.data;
+		});
+	}
 
 	$scope.onKeyPress = function($event) {
 		if ($event.keyCode === 13) {
@@ -12,7 +20,11 @@ var controller = function($scope) {
 	}
 
 	$scope.onClickAdd = function() {
-		$scope.newDescription = "";
+		var data = { description: $scope.newDescription };
+		$http.post("http://localhost:8080/todo", data).then(function(response) {
+			$scope.refresh();
+			$scope.newDescription = "";
+		});
 	}
 }
 
